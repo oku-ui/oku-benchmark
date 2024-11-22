c18a70e5c0a5cff78f9480754f7a66c3cedd1762
productdevbook <hi@productdevbook.com>
2024-11-22 11:58:55 +0300

    fix: radix-vue and reka-ui

--- frameworks/keyed/radix-vue/vite.config.js
+++ frameworks/keyed/radix-vue/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/frameworks/keyed/reka-ui/dist/',
+  base: '/frameworks/keyed/radix-vue/dist/',
   plugins: [vue()],
 })
